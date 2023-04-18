44f6096d1097138cfea504c6ee7fdbcb8470d699
hadryyassine <hadryyassine@gmail.com>
2023-04-18 00:17:38 +0000

    suppr footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styled, { ThemeProvider } from 'styled-components';
-import { Head, Loader, Nav, Social, Email, Footer } from '@components';
+import { Head, Loader, Nav, Social, Email } from '@components';
 import { GlobalStyle, theme } from '@styles';
 
 const StyledContent = styled.div`
@@ -66,10 +66,7 @@ const Layout = ({ children, location }) => {
               <Social isHome={isHome} />
               <Email isHome={isHome} />
 
-              <div id="content">
-                {children}
-                <Footer />
-              </div>
+              <div id="content">{children}</div>
             </StyledContent>
           )}
         </ThemeProvider>
